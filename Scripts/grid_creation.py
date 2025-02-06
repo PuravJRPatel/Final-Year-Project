f437f8763fd87d2565ab7713aa4e13fd9d80e661
Purav Patel <puravpatel1314@gmail.com>
2025-02-06 13:07:02 +0530

    Signed-off-by: Purav Patel <puravpatel1314@gmail.com> Streamlined Project Outline. Implemented grid structuring of India's region. Saved as grid.shp

---
+++ Scripts/grid_creation.py
@@ -0,0 +1,21 @@
+import geopandas as gpd
+import numpy as np
+from shapely.geometry import box
+
+def create_grid(region_shapefile, grid_res = 0.25):
+  region = gpd.read_file(region_shapefile)
+  minx, miny, maxx, maxy = region.total_bounds
+
+  x_coords = np.arange(minx, maxx, grid_res)
+  y_coords = np.arange(miny, maxy, grid_res)
+
+  grid_cells = [box(x, y, x + grid_res, y + grid_res) for x in x_coords for y in y_coords]
+
+  grid = gpd.GeoDataFrame({'geometry': grid_cells}, crs= region.crs)
+  grid = gpd.clip(grid, region)
+
+  return grid
+
+if __name__ == "__main__":
+  grid = create_grid(r"C:\Users\purav\OneDrive\Desktop\Fi Year Project\Final-Year-Project\Data\Shapefile\India Shape\india_ds.dbf", grid_res=0.25)
+  grid.to_file(r"C:\Users\purav\OneDrive\Desktop\Fi Year Project\Final-Year-Project\Data\Shapefile\India Shape\grid.shp")
